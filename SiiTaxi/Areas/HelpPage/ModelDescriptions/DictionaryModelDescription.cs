ab4cd2e38e1fbd66fdf7579cd38708381d7197d9
adamg <adamg@EFEKT-PC>
2017-01-31 20:07:26 +0100

    removing HelpPage, adding new View

--- SiiTaxi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SiiTaxi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
---
+++ SiiTaxi/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SiiTaxi.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
