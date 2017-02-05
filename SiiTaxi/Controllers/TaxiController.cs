c6865d1325003001051c6b152de37b011b5502a4
polishere <polishere@wp.pl>
2017-02-05 12:01:45 +0100

    change views in no authorize sections.

---
+++ SiiTaxi/Controllers/TaxiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SiiTaxi.Controllers
+{
+    public class TaxiController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+        public ActionResult Include()
+        {
+            return View();
+        }
+
+    }
+}
