6910dcadf0130b46118c5a7a20fdae338fd6aaf2
adamg <adamg@EFEKT-PC>
2017-02-05 12:35:04 +0100

    new models

---
+++ SiiTaxi/Models/People.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiiTaxi.Models
+{
+    public class People
+    {
+        [Key]
+        public int PeopleID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string OtherEmail { get; set; }
+    }
+}
---
+++ SiiTaxi/Models/Taxi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SiiTaxi.Models
+{
+    public class Taxi
+    {
+        [Key]
+        public int TaxiID { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public People Owner { get; set; }
+        public IList<People> Additional { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
