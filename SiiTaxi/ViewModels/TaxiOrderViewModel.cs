7b2b0e6a5405b36dcef0c95ec5f3735cfa9e1a1a
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2017-02-07 17:59:28 +0100

    added OrderTaxiViewModel

---
+++ SiiTaxi/ViewModels/TaxiOrderViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiiTaxi.ViewModels
+{
+    public class TaxiOrderViewModel
+    {
+        public string OwnerName { get; set; }
+
+        public string OwnerPhone { get; set; }
+
+        public string OwnerEmail { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Approver { get; set; }
+
+        public bool IsBigTaxi { get; set; }
+
+        public IList<String> PassangerList { get; set; }
+
+    }
+}
