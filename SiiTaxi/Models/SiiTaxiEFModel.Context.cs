33f29ceea0df5ef494adcb97c58d7d6459915303
polishere <polishere@wp.pl>
2017-02-11 13:04:06 +0100

    fix

--- SiiTaxi/Models/SiiTaxiEFModel.Context.cs
+++ SiiTaxi/Models/SiiTaxiEFModel.Context.cs
@@ -18,7 +18,7 @@ namespace SiiTaxi.Models
         public SiiTaxiEntities()
             : base("name=SiiTaxiEntities")
         {
-            Database.SetInitializer(new DropCreateDatabaseAlways<SiiTaxiEntities>());
+            Database.SetInitializer(new CreateDatabaseIfNotExists<SiiTaxiEntities>());
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
